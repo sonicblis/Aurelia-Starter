217071fa2b05b711ef326bdcd2f70b31efc1ce1e
Chris <slashdrew@gmail.com>
2016-09-29 11:06:42 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+import {Hello} from 'hello.js';
+
+export class App{
+	message = 'I got this crap to work';
+}
---
+++ hello.js
@@ -0,0 +1,6 @@
+import {customElement} from 'aurelia-framework';
+
+export class Hello{
+
+	greeting = 'Howdy';
+}
